a578e707d5fab9f8e108e03eb054757b79cabd70
Eric <377928044@qq.com>
2021-09-12 21:01:07 +0800

    adapter pattern

---
+++ src/section/catalog/structural/adapter/SquarePeg.java
@@ -0,0 +1,15 @@
+package section.catalog.structural.adapter;
+
+public class SquarePeg {
+
+    private double width;
+
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+}
